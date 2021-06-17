e05baaec1f2220007ca2e9acd0110f2fb61c19dc
rameshsahu <ramesh2543270@gmail.com>
2021-06-17 15:32:15 -0400

    Updated TestUtils

--- starter_code/src/test/java/com/example/demo/TestUtils.java
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -8,7 +8,8 @@ public class TestUtils {
 
         try {
             Field f = target.getClass().getDeclaredField(fieldName);
-            if (!f.canAccess(target)){
+            //if (!f.canAccess(target)){
+            if (!f.isAccessible()){
                 f.setAccessible(true);
                 wasPrivate = true;
             }
